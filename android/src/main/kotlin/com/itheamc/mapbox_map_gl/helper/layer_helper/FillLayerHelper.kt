d62d79ba9c4256230908385e66ef6f3436d0ae7b
Amit Chaudhary <itheamc@gmail.com>
2022-10-06 14:31:22 +0545

    Added FillLayer and LineLayer Helper Object

---
+++ android/src/main/kotlin/com/itheamc/mapbox_map_gl/helper/layer_helper/FillLayerHelper.kt
@@ -0,0 +1,201 @@
+package com.itheamc.mapbox_map_gl.helper.layer_helper
+
+import com.mapbox.maps.extension.style.expressions.generated.Expression
+import com.mapbox.maps.extension.style.layers.generated.FillLayerDsl
+import com.mapbox.maps.extension.style.layers.properties.generated.*
+import com.mapbox.maps.extension.style.types.StyleTransition
+import java.util.*
+
+/**
+ * FillLayerHelper.kt
+ *
+ * Created by Amit Chaudhary, 2022/10/6
+ */
+object FillLayerHelper {
+
+    /**
+     * Method to set properties got from the flutter side to FillLayerDsl block
+     */
+    fun blockFromArgs(args: Map<*, *>): FillLayerDsl.() -> Unit {
+        return {
+            // fillColor
+            if (args.containsKey("fillColor")) {
+                when (val color = args["fillColor"]) {
+                    is String -> if (color.contains("[") && color.contains("]")) {
+                        fillColor(Expression.fromRaw(color))
+                    } else {
+                        fillColor(color)
+                    }
+                    is Int -> fillColor(color)
+                }
+            }
+            // fillColorTransition
+            if (args.containsKey("fillColorTransition")) {
+                val transition = args["fillColorTransition"] as Map<*, *>
+                fillColorTransition(
+                    StyleTransition.Builder()
+                        .delay((transition["delay"] as Int).toLong())
+                        .duration((transition["duration"] as Int).toLong())
+                        .build()
+                )
+            }
+
+            // fillAntialias
+            if (args.containsKey("fillAntialias")) {
+                when (val antiAlias = args["fillAntialias"]) {
+                    is String -> if (antiAlias.contains("[") && antiAlias.contains("]")) {
+                        fillAntialias(Expression.fromRaw(antiAlias))
+                    }
+                    is Boolean -> fillAntialias(antiAlias)
+                }
+            }
+
+            // fillOpacity
+            if (args.containsKey("fillOpacity")) {
+                when (val opacity = args["fillOpacity"]) {
+                    is String -> if (opacity.contains("[") && opacity.contains("]")) {
+                        fillOpacity(Expression.fromRaw(opacity))
+                    }
+                    is Double -> fillOpacity(opacity)
+                }
+            }
+            // fillOpacityTransition
+            if (args.containsKey("fillOpacityTransition")) {
+                val transition = args["fillOpacityTransition"] as Map<*, *>
+                fillOpacityTransition(
+                    StyleTransition.Builder()
+                        .delay((transition["delay"] as Int).toLong())
+                        .duration((transition["duration"] as Int).toLong())
+                        .build()
+                )
+            }
+
+            // fillOutlineColor
+            if (args.containsKey("fillOutlineColor")) {
+                when (val color = args["fillOutlineColor"]) {
+                    is String -> if (color.contains("[") && color.contains("]")) {
+                        fillOutlineColor(Expression.fromRaw(color))
+                    } else {
+                        fillOutlineColor(color)
+                    }
+                    is Int -> fillOutlineColor(color)
+                }
+            }
+            // fillOutlineColorTransition
+            if (args.containsKey("fillOutlineColorTransition")) {
+                val transition = args["fillOutlineColorTransition"] as Map<*, *>
+                fillOutlineColorTransition(
+                    StyleTransition.Builder()
+                        .delay((transition["delay"] as Int).toLong())
+                        .duration((transition["duration"] as Int).toLong())
+                        .build()
+                )
+            }
+
+            // fillPattern
+            if (args.containsKey("fillPattern")) {
+                val pattern = args["fillPattern"] as String
+
+                if (pattern.contains("[") && pattern.contains("]")) {
+                    fillPattern(Expression.fromRaw(pattern))
+                } else {
+                    fillPattern(pattern)
+                }
+            }
+
+            // fillPatternTransition
+            if (args.containsKey("fillPatternTransition")) {
+                val transition = args["fillPatternTransition"] as Map<*, *>
+                fillPatternTransition(
+                    StyleTransition.Builder()
+                        .delay((transition["delay"] as Int).toLong())
+                        .duration((transition["duration"] as Int).toLong())
+                        .build()
+                )
+            }
+
+
+            // fillSortKey
+            if (args.containsKey("fillSortKey")) {
+                when (val sortKey = args["fillSortKey"]) {
+                    is String -> if (sortKey.contains("[") && sortKey.contains("]")) {
+                        fillSortKey(Expression.fromRaw(sortKey))
+                    }
+                    is Double -> fillSortKey(sortKey)
+                }
+            }
+
+            // fillTranslate
+            if (args.containsKey("fillTranslate")) {
+                when (val translate = args["fillTranslate"]) {
+                    is String -> if (translate.contains("[") && translate.contains("]")) {
+                        fillTranslate(Expression.fromRaw(translate))
+                    }
+                    is List<*> -> if (translate.first() is Double) fillTranslate(
+                        translate.map { it as Double }
+                    )
+                }
+            }
+
+            // fillTranslateTransition
+            if (args.containsKey("fillTranslateTransition")) {
+                val transition = args["fillTranslateTransition"] as Map<*, *>
+                fillTranslateTransition(
+                    StyleTransition.Builder()
+                        .delay((transition["delay"] as Int).toLong())
+                        .duration((transition["duration"] as Int).toLong())
+                        .build()
+                )
+            }
+
+            // fillTranslateAnchor
+            if (args.containsKey("fillTranslateAnchor")) {
+                val translateAnchor = args["fillTranslateAnchor"] as String
+
+                if (translateAnchor.contains("[") && translateAnchor.contains("]")) {
+                    fillTranslateAnchor(Expression.fromRaw(translateAnchor))
+                } else {
+                    fillTranslateAnchor(
+                        FillTranslateAnchor.valueOf(
+                            translateAnchor.uppercase(
+                                Locale.ENGLISH
+                            ).replace("-", "_")
+                        )
+                    )
+                }
+            }
+
+            // sourceLayer
+            if (args.containsKey("sourceLayer")) {
+                val sourceLayer = args["sourceLayer"] as String
+                sourceLayer(sourceLayer)
+            }
+
+            // filter
+            if (args.containsKey("filter")) {
+                val filter = args["filter"] as String
+                if (filter.contains("[") && filter.contains("]")) {
+                    filter(Expression.fromRaw(filter))
+                }
+            }
+
+            // maxZoom
+            if (args.containsKey("maxZoom")) {
+                val max = args["maxZoom"] as Double
+                maxZoom(max)
+            }
+
+            // minZoom
+            if (args.containsKey("minZoom")) {
+                val min = args["minZoom"] as Double
+                minZoom(min)
+            }
+
+            // visibility
+            if (args.containsKey("visibility")) {
+                val visible = args["visibility"] as Boolean
+                visibility(if (visible) Visibility.VISIBLE else Visibility.NONE)
+            }
+        }
+    }
+}
---
+++ android/src/main/kotlin/com/itheamc/mapbox_map_gl/helper/layer_helper/LineLayerHelper.kt
@@ -0,0 +1,350 @@
+package com.itheamc.mapbox_map_gl.helper.layer_helper
+
+import com.mapbox.maps.extension.style.expressions.generated.Expression
+import com.mapbox.maps.extension.style.layers.generated.LineLayerDsl
+import com.mapbox.maps.extension.style.layers.properties.generated.LineCap
+import com.mapbox.maps.extension.style.layers.properties.generated.LineJoin
+import com.mapbox.maps.extension.style.layers.properties.generated.LineTranslateAnchor
+import com.mapbox.maps.extension.style.layers.properties.generated.Visibility
+import com.mapbox.maps.extension.style.types.StyleTransition
+import java.util.*
+
+/**
+ * LineLayerHelper.kt
+ *
+ * Created by Amit Chaudhary, 2022/10/6
+ */
+object LineLayerHelper {
+
+    /**
+     * Method to set properties got from the flutter side to LineLayerDsl block
+     */
+    fun blockFromArgs(args: Map<*, *>): LineLayerDsl.() -> Unit {
+        return {
+            // lineColor
+            if (args.containsKey("lineColor")) {
+                when (val color = args["lineColor"]) {
+                    is String -> if (color.contains("[") && color.contains("]")) {
+                        lineColor(Expression.fromRaw(color))
+                    } else {
+                        lineColor(color)
+                    }
+                    is Int -> lineColor(color)
+                }
+            }
+            // lineColorTransition
+            if (args.containsKey("lineColorTransition")) {
+                val transition = args["lineColorTransition"] as Map<*, *>
+                lineColorTransition(
+                    StyleTransition.Builder()
+                        .delay((transition["delay"] as Int).toLong())
+                        .duration((transition["duration"] as Int).toLong())
+                        .build()
+                )
+            }
+
+            // lineWidth
+            if (args.containsKey("lineWidth")) {
+                when (val width = args["lineWidth"]) {
+                    is String -> if (width.contains("[") && width.contains("]")) {
+                        lineWidth(Expression.fromRaw(width))
+                    }
+                    is Double -> lineWidth(width)
+                }
+            }
+            // lineWidthTransition
+            if (args.containsKey("lineWidthTransition")) {
+                val transition = args["lineWidthTransition"] as Map<*, *>
+                lineWidthTransition(
+                    StyleTransition.Builder()
+                        .delay((transition["delay"] as Int).toLong())
+                        .duration((transition["duration"] as Int).toLong())
+                        .build()
+                )
+            }
+
+            // lineBlur
+            if (args.containsKey("lineBlur")) {
+                when (val blur = args["lineBlur"]) {
+                    is String -> if (blur.contains("[") && blur.contains("]")) {
+                        lineBlur(Expression.fromRaw(blur))
+                    }
+                    is Double -> lineBlur(blur)
+                }
+            }
+            // lineBlurTransition
+            if (args.containsKey("lineBlurTransition")) {
+                val transition = args["lineBlurTransition"] as Map<*, *>
+                lineBlurTransition(
+                    StyleTransition.Builder()
+                        .delay((transition["delay"] as Int).toLong())
+                        .duration((transition["duration"] as Int).toLong())
+                        .build()
+                )
+            }
+
+            // lineOpacity
+            if (args.containsKey("lineOpacity")) {
+                when (val opacity = args["lineOpacity"]) {
+                    is String -> if (opacity.contains("[") && opacity.contains("]")) {
+                        lineOpacity(Expression.fromRaw(opacity))
+                    }
+                    is Double -> lineOpacity(opacity)
+                }
+            }
+            // lineOpacityTransition
+            if (args.containsKey("lineOpacityTransition")) {
+                val transition = args["lineOpacityTransition"] as Map<*, *>
+                lineOpacityTransition(
+                    StyleTransition.Builder()
+                        .delay((transition["delay"] as Int).toLong())
+                        .duration((transition["duration"] as Int).toLong())
+                        .build()
+                )
+            }
+
+            // lineDashArray
+            if (args.containsKey("lineDashArray")) {
+                when (val dashArray = args["lineDashArray"]) {
+                    is String -> if (dashArray.contains("[") && dashArray.contains("]")) {
+                        lineDasharray(Expression.fromRaw(dashArray))
+                    }
+                    is List<*> -> if (dashArray.first() is Double) lineDasharray(
+                        dashArray.map { it as Double }
+                    )
+                }
+            }
+
+            // lineDashArrayTransition
+            if (args.containsKey("lineDashArrayTransition")) {
+                val transition = args["lineDashArrayTransition"] as Map<*, *>
+                lineDasharrayTransition(
+                    StyleTransition.Builder()
+                        .delay((transition["delay"] as Int).toLong())
+                        .duration((transition["duration"] as Int).toLong())
+                        .build()
+                )
+            }
+
+            // lineGapWidth
+            if (args.containsKey("lineGapWidth")) {
+                when (val gapWidth = args["lineGapWidth"]) {
+                    is String -> if (gapWidth.contains("[") && gapWidth.contains("]")) {
+                        lineGapWidth(Expression.fromRaw(gapWidth))
+                    }
+                    is Double -> lineGapWidth(gapWidth)
+                }
+            }
+
+            // lineGapWidthTransition
+            if (args.containsKey("lineGapWidthTransition")) {
+                val transition = args["lineGapWidthTransition"] as Map<*, *>
+                lineGapWidthTransition(
+                    StyleTransition.Builder()
+                        .delay((transition["delay"] as Int).toLong())
+                        .duration((transition["duration"] as Int).toLong())
+                        .build()
+                )
+            }
+
+            // lineGradient
+            if (args.containsKey("lineGradient")) {
+                val gapWidth = args["lineGradient"] as String
+                if (gapWidth.contains("[") && gapWidth.contains("]")) {
+                    lineGradient(Expression.fromRaw(gapWidth))
+                }
+            }
+
+            // lineMiterLimit
+            if (args.containsKey("lineMiterLimit")) {
+                when (val miterLimit = args["lineMiterLimit"]) {
+                    is String -> if (miterLimit.contains("[") && miterLimit.contains("]")) {
+                        lineMiterLimit(Expression.fromRaw(miterLimit))
+                    }
+                    is Double -> lineMiterLimit(miterLimit)
+                }
+            }
+
+            // lineOffset
+            if (args.containsKey("lineOffset")) {
+                when (val offset = args["lineOffset"]) {
+                    is String -> if (offset.contains("[") && offset.contains("]")) {
+                        lineOffset(Expression.fromRaw(offset))
+                    }
+                    is Double -> lineOffset(offset)
+                }
+            }
+
+            // lineOffsetTransition
+            if (args.containsKey("lineOffsetTransition")) {
+                val transition = args["lineOffsetTransition"] as Map<*, *>
+                lineOffsetTransition(
+                    StyleTransition.Builder()
+                        .delay((transition["delay"] as Int).toLong())
+                        .duration((transition["duration"] as Int).toLong())
+                        .build()
+                )
+            }
+
+            // linePattern
+            if (args.containsKey("linePattern")) {
+                val pattern = args["linePattern"] as String
+
+                if (pattern.contains("[") && pattern.contains("]")) {
+                    linePattern(Expression.fromRaw(pattern))
+                } else {
+                    linePattern(pattern)
+                }
+            }
+
+            // linePatternTransition
+            if (args.containsKey("linePatternTransition")) {
+                val transition = args["linePatternTransition"] as Map<*, *>
+                linePatternTransition(
+                    StyleTransition.Builder()
+                        .delay((transition["delay"] as Int).toLong())
+                        .duration((transition["duration"] as Int).toLong())
+                        .build()
+                )
+            }
+
+            // lineRoundLimit
+            if (args.containsKey("lineRoundLimit")) {
+                when (val roundLimit = args["lineRoundLimit"]) {
+                    is String -> if (roundLimit.contains("[") && roundLimit.contains("]")) {
+                        lineRoundLimit(Expression.fromRaw(roundLimit))
+                    }
+                    is Double -> lineRoundLimit(roundLimit)
+                }
+            }
+
+            // lineSortKey
+            if (args.containsKey("lineSortKey")) {
+                when (val sortKey = args["lineSortKey"]) {
+                    is String -> if (sortKey.contains("[") && sortKey.contains("]")) {
+                        lineSortKey(Expression.fromRaw(sortKey))
+                    }
+                    is Double -> lineSortKey(sortKey)
+                }
+            }
+
+            // lineTranslate
+            if (args.containsKey("lineTranslate")) {
+                when (val lineTranslate = args["lineTranslate"]) {
+                    is String -> if (lineTranslate.contains("[") && lineTranslate.contains("]")) {
+                        lineTranslate(Expression.fromRaw(lineTranslate))
+                    }
+                    is List<*> -> if (lineTranslate.first() is Double) lineTranslate(
+                        lineTranslate.map { it as Double }
+                    )
+                }
+            }
+
+            // lineTranslateTransition
+            if (args.containsKey("lineTranslateTransition")) {
+                val transition = args["lineTranslateTransition"] as Map<*, *>
+                lineTranslateTransition(
+                    StyleTransition.Builder()
+                        .delay((transition["delay"] as Int).toLong())
+                        .duration((transition["duration"] as Int).toLong())
+                        .build()
+                )
+            }
+
+            // lineTranslateAnchor
+            if (args.containsKey("lineTranslateAnchor")) {
+                val translateAnchor = args["lineTranslateAnchor"] as String
+
+                if (translateAnchor.contains("[") && translateAnchor.contains("]")) {
+                    lineTranslateAnchor(Expression.fromRaw(translateAnchor))
+                } else {
+                    lineTranslateAnchor(
+                        LineTranslateAnchor.valueOf(
+                            translateAnchor.uppercase(
+                                Locale.ENGLISH
+                            ).replace("-", "_")
+                        )
+                    )
+                }
+            }
+
+            // lineTrimOffset
+            if (args.containsKey("lineTrimOffset")) {
+                when (val trimOffset = args["lineTrimOffset"]) {
+                    is String -> if (trimOffset.contains("[") && trimOffset.contains("]")) {
+                        lineTrimOffset(Expression.fromRaw(trimOffset))
+                    }
+                    is List<*> -> if (trimOffset.first() is Double) lineTrimOffset(
+                        trimOffset.map { it as Double }
+                    )
+                }
+            }
+
+            // lineCap
+            if (args.containsKey("lineCap")) {
+                val cap = args["lineCap"] as String
+
+                if (cap.contains("[") && cap.contains("]")) {
+                    lineCap(Expression.fromRaw(cap))
+                } else {
+                    lineCap(
+                        LineCap.valueOf(
+                            cap.uppercase(
+                                Locale.ENGLISH
+                            ).replace("-", "_")
+                        )
+                    )
+                }
+            }
+
+            // lineJoin
+            if (args.containsKey("lineJoin")) {
+                val translateAnchor = args["lineJoin"] as String
+
+                if (translateAnchor.contains("[") && translateAnchor.contains("]")) {
+                    lineJoin(Expression.fromRaw(translateAnchor))
+                } else {
+                    lineJoin(
+                        LineJoin.valueOf(
+                            translateAnchor.uppercase(
+                                Locale.ENGLISH
+                            ).replace("-", "_")
+                        )
+                    )
+                }
+            }
+
+            // sourceLayer
+            if (args.containsKey("sourceLayer")) {
+                val sourceLayer = args["sourceLayer"] as String
+                sourceLayer(sourceLayer)
+            }
+
+            // filter
+            if (args.containsKey("filter")) {
+                val filter = args["filter"] as String
+                if (filter.contains("[") && filter.contains("]")) {
+                    filter(Expression.fromRaw(filter))
+                }
+            }
+
+            // maxZoom
+            if (args.containsKey("maxZoom")) {
+                val max = args["maxZoom"] as Double
+                maxZoom(max)
+            }
+
+            // minZoom
+            if (args.containsKey("minZoom")) {
+                val min = args["minZoom"] as Double
+                minZoom(min)
+            }
+
+            // visibility
+            if (args.containsKey("visibility")) {
+                val visible = args["visibility"] as Boolean
+                visibility(if (visible) Visibility.VISIBLE else Visibility.NONE)
+            }
+        }
+    }
+}
