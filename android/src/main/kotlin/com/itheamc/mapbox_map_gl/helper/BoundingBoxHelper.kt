061aa62606ef7f02ae47baf999f98934e4b3c0c0
Amit Chaudhary <itheamc@gmail.com>
2022-10-06 14:20:21 +0545

    Added BoundingBox and Point Helper object

---
+++ android/src/main/kotlin/com/itheamc/mapbox_map_gl/helper/BoundingBoxHelper.kt
@@ -0,0 +1,37 @@
+package com.itheamc.mapbox_map_gl.helper
+
+import com.mapbox.geojson.BoundingBox
+
+/**
+ * BoundingBoxHelper.kt
+ *
+ * Created by Amit Chaudhary, 2022/10/5
+ *
+ * BoundingBoxHelper to deal with bounding box related data
+ */
+object BoundingBoxHelper {
+
+    /**
+     * Method to convert arguments to BoundingBox object
+     * params:
+     * {args} - > Map<*, *> data
+     */
+    fun fromArgs(args: Map<*, *>?): BoundingBox? {
+
+        args?.let {
+            val southwest = it["southwest"] as Map<*, *>
+            val northeast = it["northeast"] as Map<*, *>
+
+            return PointHelper.fromArgs(southwest)?.let { it1 ->
+                PointHelper.fromArgs(northeast)?.let { it2 ->
+                    BoundingBox.fromPoints(
+                        it1,
+                        it2
+                    )
+                }
+            }
+        }
+
+        return null
+    }
+}
---
+++ android/src/main/kotlin/com/itheamc/mapbox_map_gl/helper/PointHelper.kt
@@ -0,0 +1,34 @@
+package com.itheamc.mapbox_map_gl.helper
+
+import com.mapbox.geojson.Point
+
+/**
+ * PointHelper.kt
+ *
+ * Created by Amit Chaudhary, 2022/10/5
+ *
+ * PointHelper to deal with point related data
+ */
+object PointHelper {
+
+    /**
+     * Method to convert arguments to Point object
+     * params:
+     * {args} - > Map<*, *> data
+     */
+    fun fromArgs(args: Map<*, *>?): Point? {
+
+        args?.let {
+            val coordinates = it["coordinates"] as List<*>
+            val bbox = it["bbox"] as Map<*, *>?
+
+            return Point.fromLngLat(
+                (coordinates).first() as Double,
+                (coordinates).last() as Double,
+                BoundingBoxHelper.fromArgs(bbox)
+            )
+        }
+
+        return null
+    }
+}
