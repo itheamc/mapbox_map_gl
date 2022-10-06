07b889e72db9d697d79fe8cf206d1dd049b153ea
Amit Chaudhary <itheamc@gmail.com>
2022-10-06 20:45:38 +0545

    added feature collection class

---
+++ lib/src/helper/feature_collection.dart
@@ -0,0 +1,36 @@
+import '../../mapbox_map_gl.dart';
+
+/// FeatureCollection Class
+/// Created by Amit Chaudhary, 2022/10/6
+class FeatureCollection {
+  static const _type = "FeatureCollection";
+
+  /// [type] FeatureCollection Type i.e. 'FeatureCollection'
+  final String type;
+
+  /// [features] List of Feature
+  final List<Feature> features;
+
+  /// [bbox] Bonding box
+  final BoundingBox? bbox;
+
+  /// A private constructor
+  FeatureCollection._(this.type, this.features, [this.bbox]);
+
+  /// Factory method to create feature collection
+  factory FeatureCollection.fromFeatures({
+    required List<Feature> features,
+    BoundingBox? bbox,
+  }) {
+    return FeatureCollection._(_type, features, bbox);
+  }
+
+  /// Method to convert FeatureCollection to map
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      "type": type,
+      "features": features.map((e) => e.toMap()).toList(),
+      "bbox": bbox?.toMap()
+    };
+  }
+}
